2ffccad5bca8c1373f986b446d510b4f2af1bbbb
serviolee <serviolee@gmail.com>
2020-03-31 13:38:19 -0700

    updated img to pacman

---
+++ src/blinkyGhostDancer.js
@@ -0,0 +1,21 @@
+var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+  this.$node.html('<img class="ghost" src="https://static.giantbomb.com/uploads/square_small/8/87790/2469740-blinky.png"/>');
+
+};
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+makeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
