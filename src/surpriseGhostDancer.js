6b0f09abb4549cc32cc22ff42b7534fb4976e8d2
serviolee <serviolee@gmail.com>
2020-03-31 13:39:31 -0700

    created a makeSurpriseDancer constructor

---
+++ src/surpriseGhostDancer.js
@@ -0,0 +1,21 @@
+var makeSurpriseDancer = function(top, left, timeBetweenSteps) {
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+  this.$node.addClass('surprise');
+  this.$node.html('<img class="ghost" src="https://static.giantbomb.com/uploads/square_small/8/87790/2469741-inky.png"/>');
+
+
+};
+
+makeSurpriseDancer.prototype = Object.create(makeDancer.prototype);
+
+makeSurpriseDancer.prototype.constructor = makeSurpriseDancer;
+
+makeSurpriseDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+
+  // Add spin functionality
+  this.$node.toggleClass('rotateY');
+
+};
