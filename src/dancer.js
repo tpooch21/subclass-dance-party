cd2167871d5f44de9709e259a2f4460d483912be
serviolee <serviolee@gmail.com>
2020-03-30 20:17:28 -0700

    fixed makeDancer to correctly create dancer object

--- src/dancer.js
+++ src/dancer.js
@@ -1,39 +1,21 @@
-// Refactor makeDancer superclass to pseudoclassical
-// Pseudoclassical
-//  Use 'this' keyword to add and point to properties and methods
-//  Uses prototypal chain for methods that are shared amongst instances of a class
-//    Uses prototype object of class constructor function to store methods
-//  Uses keyword 'new' to instantiate an object
-
-// Don't need to create var dancer
-// Properties we can use keyword 'this'
-// Methods - we can store them in makeDancer's prototype object
-// No return statement
-
-// Creates and returns a new dancer object that can step
-var MakeDancer = function(top, left, timeBetweenSteps) {
-
-  // use jQuery to create an HTML <span> tag
+var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
-
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
   this.step();
-
-  // now that we have defined the this dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
 };
 
-MakeDancer.prototype.step = function() {
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+makeDancer.prototype.step = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps); // schedules the next step
 };
 
-MakeDancer.prototype.setPosition = function(top, left) {
+makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
-  var styleSettings = {
+  styleSettings = {
     top: top,
     left: left
   };
