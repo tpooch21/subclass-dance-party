00b64927ebf5709639184074f10b718c241a8fbf
serviolee <serviolee@gmail.com>
2020-03-31 13:41:20 -0700

    created pacmanDancer constructor

---
+++ src/pacmanDancer.js
@@ -0,0 +1,22 @@
+var makeBouncyDancer = function(top, left, timeBetweenSteps, lineUp) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+  this.$node.addClass('pacman');
+  this.$node.html('<img class="pacman" src="https://upload.wikimedia.org/wikipedia/commons/4/49/Pacman.svg"/>')
+  this.$node.addClass('lineUp');
+
+};
+
+makeBouncyDancer.prototype = Object.create(makeDancer.prototype);
+makeBouncyDancer.prototype.constructor = makeBouncyDancer;
+
+makeBouncyDancer.prototype.step = function() {
+  makeDancer.prototype.step.call(this);
+
+  if (this.$node.hasClass('lineUp')) {
+    this.$node.animate({
+      left: '+=100'
+    }, 1000);
+  }
+};
+
